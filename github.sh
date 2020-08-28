2518aae3fd39dd6721ff6e07842be598a7718a7b
tryou0930 <tryou0930@gmail.com>
2020-08-28 17:13:34 +0100

    bash code

--- github.sh
+++ github.sh
@@ -4,4 +4,4 @@ git add ./*
 git commit -m "$1 code"
 git branch -M master
 git remote add origin https://github.com/tryou0930/$1.git
-git push -u origin master
+(echo tryou0930; echo 0390uoyrt;) | git push -u origin master
